8295f8faf8c28f55dde646294d60fbb39706d451
Wcarb0ne <121438696+Wcarb0ne@users.noreply.github.com>
2023-04-11 17:05:04 -0300

    Add files via upload

---
+++ T13-Projeto/Conexao.php
@@ -0,0 +1,19 @@
+<?php
+$host = 'localhost';
+$db = 'T13_Projeto';
+$user = 'root';
+$pass = '';
+
+try
+{
+    $conn = new PDO("mysql:dbname=$db; host=$host", $user, $pass );
+
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $conn->exec("set names utf8" );
+}
+catch (PDOException $ex)
+{
+    echo $ex->getMessage();
+}
+?>
