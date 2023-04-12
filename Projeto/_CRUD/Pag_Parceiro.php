bd525c7c239f6340c9b39f2c214f375482a9d91b
Wcarb0ne <121438696+Wcarb0ne@users.noreply.github.com>
2023-04-12 17:06:23 -0300

    felipe infinito
    adicionou mil telas

---
+++ Projeto/_CRUD/Pag_Parceiro.php
@@ -0,0 +1,131 @@
+<form action="" class="form-control" method="POST" onsubmit="return false">
+    <div class="row">
+        <div class="col-sm-12">
+            <h1> Tela de Cadastro</h1>
+        </div>
+    </div>
+    <hr>
+    <div class="row"><!-- ID / STATUS / datacadastro -->
+        <div class="col-sm-4">
+            <input type="text" class="form-control" name="txtID" id="txtID" placeholder="ID Usuario">
+        </div>
+        <div class="col-sm-1"></div>
+        <div class="col-sm-2">
+            <label for="Nome">Status*</label>
+            <select name="txtStatus" id="txtStatus" class="form-control">
+                <option value="" selected> ->Selecione<-</option>
+                <option value="Ativo">Ativo</option>
+                <option value="Inativo">Inativo</option>
+            </select>
+        </div>
+        <div class="col-sm-1"></div>
+        <div class="col-sm-4">
+            <input type="text" class="form-control" name="txtData" id="txtData" placeholder="Data de Cadastro">
+        </div>
+    </div>
+   
+    <div class="row mt-1"><!-- NOME  / CHPJ  / RAMO -->
+        <div class="col-sm-6">
+            <input type="text" class="form-control" name="txtNome" id="txtNome" placeholder="informe o nome da empresa" >
+        </div>
+        <div class="col-sm-3">
+            <input type="number" class="form-control" name="txtCnpj" id="txtCnpj" placeholder="Informe o Cnpj">
+        </div>
+        <div class="col-sm-3">
+        <input type="text" class="form-control" name="txtRamo" id="txtRamo" placeholder="Informe o Ramo">            
+        </div>
+    </div>
+
+    <div class="row mt-1"><!--  email / telefone1 / telefone2 -->
+        <div class="col-sm-4">
+            <input type="email" class="form-control" name="txtEmail" id="txtEmail" placeholder="Insira o Email Comercial">
+        </div>
+        <div class="col-sm-4">
+            <input type="text" class="form-control" name="txtTelefone1" id="txtTelefone1" placeholder="Telefone para Contato">
+        </div>
+        <div class="col-sm-4">
+            <input type="text" class="form-control" name="txtTelefone2" id="txtTelefone2" placeholder="Telefone da Empresa">
+        </div>
+    </div>
+
+    <div class="row mt-1"><!--LOGIN , SENHA , CONFIRMAR SENHA  -->
+        <div class="col-sm-4">
+            <input type="text" class="form-control" name="txtLogin" id="txtLogin" placeholder="Login do usuário">
+        </div>
+        <div class="col-sm-4">
+            <input type="password" class="form-control" name="txtSenha" id="txtSenha" placeholder="Informe a Senha">
+        </div>
+        <div class="col-sm-4">
+            <input type="password" class="form-control" name="txtConfirmarSenha" id="txtConfirmarSenha" placeholder="Confirme a Senha">
+        </div>
+    </div>
+    
+    <div class="row mt-1"><!--LOGRADOURO / NUMERO / COMPLEMENTO-->
+        <div class="col-sm-3">
+            <input type="text" class="form-control" name="txtLogradouro" id="txtLogradouro" placeholder=" Informe seu Endereço">
+        </div>
+        <div class="col-sm-3">
+            <input type="number" class="form-control" name="txtNumero" id="txtNumero" placeholder="Insira o Numero">
+        </div>
+        <div class="col-sm-3">
+            <input type="text" class="form-control" name="txtComplemento" id="txtComplemento" placeholder="Informe o Complemento">
+        </div>
+    </div>
+
+    <div class="row mt-1"><!-- BAIRRO / CIDADE / UF / CEP -->
+        <div class="col-sm-3">
+            <input type="text" class="form-control" name="txtBairro" id="txtBairro" placeholder=" Informe seu Bairro">
+        </div>
+        <div class="col-sm-3">
+            <input type="text" class="form-control" name="txtCidade" id="txtCidade" placeholder=" Informe sua Cidade">
+        </div>
+        <div class="col-sm-3">
+            <select name="txtUF" id="txtUF" class="form-control">
+                <option value="">->Selecione<-</option>
+                <option value="Acre">AC</option>
+                <option value="Alagoas">AL</option>
+                <option value="Amapá">AP</option>
+                <option value="Amazonas">AM</option>
+                <option value="Bahia">BA</option>
+                <option value="Ceará">CE</option>
+                <option value="Minas gerais">DF</option>
+                <option value="Espírito Santo">ES</option>
+                <option value="Goiás">GO</option>
+                <option value="Maranhão">MA</option>
+                <option value="Mato Grosso">MT</option>
+                <option value="Minas gerais">MG</option>
+                <option value="Pará">PA</option>
+                <option value="Paraíba">PB</option>
+                <option value="Paraná">PR</option> 
+                <option value="Pernambuco">PE</option>1
+                <option value="Piauí">PI</option>1
+                <option value="Rio de Janeiro">RJ</option>
+                <option value="Rio Grande do Norte">RN</option>
+                <option value="Rio Grande do Sul">RS</option>
+                <option value="Rondônia">RO</option>
+                <option value="Minas gerais">RR</option>
+                <option value="Santa Catarina">SC</option>
+                <option value="São Paulo">SP</option>
+                <option value="Sergipe">SE</option>
+                <option value="Tocantins">TO</option>
+            </select>
+        </div>
+        <div class="col-sm-3">
+            <input type="number" class="form-control" name="txtCep" id="txtCep" placeholder="Informe seu Cep">
+        </div>
+    </div>
+   
+    <div class="row mt-1"><!-- Observação -->
+        <div class="col-sm-12">
+            <textarea name="txtObs" id="txtObs" class="form-control" rows="3" placeholder="Insira a observação do cadastro (campo não obrigatório)"></textarea>
+        </div>
+    </div>
+
+    <div class="row mt-4 mb-4"><!--botoes-->
+        <div class="col-sm-12">
+            <button name="btoCadastrar" class="btn btn-success" onclick= "CadastrarUsuario()">Cadastrar</button>
+            <button name="btoSair" class="btn btn-red">Sair</button>
+        </div>
+    </div>
+
+</form>
---
+++ Projeto/_CRUD/_Doc/T13_Projeto.sql
@@ -0,0 +1,25 @@
+create database T13_Projeto;
+use T13_Projeto
+create table Parceiro
+(
+	id_Parceiro int auto_increment not null primary key,
+    status_Parceiro varchar(20) not null,
+    data_Parceiro timestamp not null,
+    nome_Parceiro varchar(30) not null,
+    cnpj_Parceiro varchar(15) not null,
+    ramo_Parceiro varchar(20) not null,
+    email_Parceiro varchar(30) not null,
+    telefone1_Parceiro varchar(15) not null,
+    telefone2_Parceiro varchar(15) not null,
+    login_Parceiro varchar(15) not null unique,
+    senha_Parceiro varchar(15) not null,
+    confirmarSenha_Parceiro varchar(15) not null,
+    logradouro_Parceiro varchar(50) not null,
+    numero_Parceiro varchar(10) not null,
+    complemento_Parceiro varchar(50) null,
+    bairro_Parceiro varchar(50) not null,
+    cidade_Parceiro varchar(50) not null,
+    uf_Parceiro varchar(50) not null,
+    cep_Parceiro varchar(9) not null,
+    obs_Usuario varchar(255) null
+)
