ed756e877539b55894fb479e4e3af8fb0e761984
Felipeglb <felipe.gabriel.lb.2018@gmail.com>
2023-04-17 17:11:03 -0300

    v8

---
+++ T13/_Projeto/landingPage.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/bootstrap.css">
+   <!--cabeçalho --> <link rel="stylesheet" href="css/headerdeuruim.css">
+
+   <!--fle conosco--> <link rel="stylesheet" href="css/feleconosco.css">
+     
+     <!--RODAPE --><link rel='stylesheet' href='https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css'>
+     <!--RODAPE --><link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css'>
+     <!--RODAPE --><link rel="stylesheet" href="css/footerAMEM.css">
+     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.4/font/bootstrap-icons.css">
+
+     
+    <script src="css/Login/js/jquery.js"></script>
+
+    <title>Document</title>
+</head>
+
+
+
+<header>
+    <div class="row">
+        <?php
+        include_once('_header.php')
+        ?>
+    </div>
+</header>
+
+<body>
+    <div class="row">
+Ao contrário da crença popular, Lorem Ipsum não é simplesmente um texto aleatório. Tem raízes em uma peça da literatura latina clássica de 45 aC, com mais de 2.000 anos de idade. Richard McClintock, professor de latim no Hampden-Sydney College, na Virgínia, pesquisou uma das palavras latinas mais obscuras, consectetur, de uma passagem de Lorem Ipsum e, examinando as citações da palavra na literatura clássica, descobriu a fonte indubitável. Lorem Ipsum vem das seções 1.10.32 e 1.10.33 de "de Finibus Bonorum et Malorum" (Os Extremos do Bem e do Mal) de Cícero, escrito em 45 AC. Este livro é um tratado sobre a teoria da ética, muito popular durante o Renascimento. A primeira linha de Lorem Ipsum, "Lorem ipsum dolor sit amet..", vem de uma linha na seção 1.10.32.
+
+O pedaço padrão de Lorem Ipsum usado desde 1500 é reproduzido abaixo para os interessados. As seções 1.10.32 e 1.10.33 de "de Finibus Bonorum et Malorum" de Cícero também são reproduzidas em sua forma original exata, acompanhadas por versões em inglês da tradução de 1914 por H. Rackham
+
+</div>
+</body>
+
+<footer class="footer">
+    <div class="container">
+        <?php include_once('_footer.php');?>
+    </div>
+</footer>
+
+
+
+
+</html>
