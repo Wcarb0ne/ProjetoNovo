dd3943990fb3d3a5fc941ad5e486fc25f370b326
Wcarb0ne <121438696+Wcarb0ne@users.noreply.github.com>
2023-04-18 16:13:32 -0300

    v7

--- T13/_Projeto/Parceiro_btoCategoriaPesquisar.php
+++ T13/_Projeto/Parceiro_btoCategoriaPesquisar.php
@@ -2,50 +2,36 @@
 include_once('Conexao.php');
 
 
-$id_PCateoria="";
-$data_PCateoria="";
-$nome_PCateoria="";
-$status_PCateoria="";
+$id_PCategoria="";
+$data_PCategoria="";
+$nome_PCategoria="";
+$status_PCategoria="";
 
 if($_POST)
 {
     $id_PCategoria =$_POST['txtID'];
     try
     {   
-        $teste = 'select * from PCateoria where id_PCateoria='.$id_PCateoria;
+        $teste = 'select * from PCategoria where id_PCategoria='.$id_PCategoria;
         $sql = $conn->query($teste);
         
         if($sql->rowCount()==1)
         {
             foreach($sql as $linha)
             {
-                $id_Parceiro=$linha[0];
-                $data_Parceiro=$linha[1];
-                $nome_Parceiro=$linha[2];
-                $status_Parceiro=$linha[3];
+                $id_PCategoria=$linha[0];
+                $data_PCategoria=$linha[1];
+                $nome_PCategoria=$linha[2];
+                $status_PCategoria=$linha[3];
                 
             }
-            // echo"
-            // <p>id_Parceiro: <span id='idGerado'>$id_Parceiro</span></p>
-            // <p>nome_Parceiro: <span id='PesquisaNome'>$nome_Parceiro</span></p>
-            // <p>login_Parceiro: <span id='PesquisaLogin'>$login_Parceiro</span></p>
-            // <p>senha_Parceiro: <span id='PesquisaSenha'>$senha_Parceiro</span></p>
-            // <p>confirmarSenha_Parceiro: <span id='PesquisaConfirmarSenha'>$confirmarSenha_Parceiro</span></p>
-            // <p>email_Parceiro: <span id='PesquisaEmail'>$email_Parceiro</span></p>
-            // <p>dataNasc_Parceiro: <span id='PesquisaDataNasc'>$dataNasc_Parceiro</span></p>
-            // <p>cpf_Parceiro: <span id='PesquisaCpf'>$cpf_Parceiro</span></p>
-            // <p>genero_Parceiro: <span id='PesquisaGenero'>$genero_Parceiro</span></p>
-            // <p>telefone1_Parceiro: <span id='PesquisaTelefone1'>$telefone1_Parceiro</span></p>
-            // <p>telefone2_Parceiro: <span id='PesquisaTelefone2'>$telefone2_Parceiro</span></p>
-            // <p>logradouro_Parceiro: <span id='PesquisaLogradouro'>$logradouro_Parceiro</span></p>
-            // <p>cidade_Parceiro: <span id='PesquisaCidade'>$cidade_Parceiro</span></p>
-            // <p>uf_Parceiro: <span id='PesquisaUF'>$uf_Parceiro</span></p>
-            // <p>cep_Parceiro: <span id='PesquisaCep'>$cep_Parceiro</span></p>
-            // <p>cadastro_Parceiro: <span id='PesquisaCadastro'>$cadastro_Parceiro</span></p>
-            // <p>status_Parceiro: <span id='PesquisaStatus'>$status_Parceiro</span></p>
-            // <p>obs_Parceiro: <span id='PesquisaObs'>$$obs_Parceiro</span></p>
+             echo"
+             <p>id_PCategoria: <span id='idGerado'>$id_PCategoria</span></p>
+             <p>data_PCategoria: <span id='PesquisaData'>$data_PCategoria</span></p>
+             <p>nome_PCategoria: <span id='PesquisaNome'>$nome_PCategoria</span></p>
+             <p>status_PCategoria: <span id='PesquisaStatus'>$status_PCategoria</span></p>
             
-            // ";
+           ";
         } 
         else
         {
