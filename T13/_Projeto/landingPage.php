4b4016bf00d6fd0d42288dc377c8450ee0fa746a
Felipeglb <felipe.gabriel.lb.2018@gmail.com>
2023-04-24 17:15:26 -0300

    v18

--- T13/_Projeto/landingPage.php
+++ T13/_Projeto/landingPage.php
@@ -1,53 +1,55 @@
-<body style="background-color:#043668;">
+<body>
 
-  <div class="container">
-    <div class="col-sm-12">
+  <div class="container" >
 
-      <head style="margin-bottom: 20;">
-
-
-        <img src="css/img/teste3.jpg" class="imagem" alt="">
+    <div class="col-sm-6">
 
 
+    </div>
 
-      </head>
+      <div class="col-sm-6"  >
+        <head style="margin-bottom: 20;" >
+          <img src="css/img/pexels-buro-millennial-1438081.jpg" class="imagem" alt="">
+        </head>
+      </div>
+ 
 
 
-      <p></p>
+    <p></p>
+    <p></p>
 
-      <div class="row">
-        <h2 style="color:white;">Planos</h2>
-        <hr style="color:white;">
-        <div class=" card-group">
+    <div class="row">
+      <h2 style="color:#043668;">Planos</h2>
+      <hr style="color:#043668;">
+      <div class=" card-group">
 
-          <div class="col-md-4 text-center mb-3 p-2">
-            <div class="card card-body">
-              <h5 class="card-title" style="font-family: 'Open Sans', sans-serif;">Special title treatment</h5>
-              <p class="card-text"style="font-family: 'Open Sans', sans-serif;">With supporting text below as a natural lead-in to additional content.</p>
-              <a href="#" class="btn btn-primary">Go somewhere</a>
-            </div>
+        <div class="col-md-4 text-center mb-3 p-2">
+          <div class="card card-body">
+            <h5 class="card-title" style="font-family: 'Open Sans', sans-serif;">Special title treatment</h5>
+            <p class="card-text" style="font-family: 'Open Sans', sans-serif;">With supporting text below as a natural lead-in to additional content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
           </div>
-
-          <div class="col-md-4 text-center mb-3 p-2">
-            <div class="card card-body" style="font-family: 'Open Sans', sans-serif;">
-              <h5 class="card-title"style="font-family: 'Open Sans', sans-serif;">Special title treatment</h5>
-              <p class="card-text"style="font-family: 'Open Sans', sans-serif;">With supporting text below as a natural lead-in to additional content.</p>
-              <a href="#" class="btn btn-primary">Go somewhere</a>
-            </div>
-          </div>
-
-          <div class="col-md-4 text-center mb-3 p-2">
-            <div class="card card-body" style="font-family: 'Open Sans', sans-serif;">
-              <h5 class="card-title" style="font-family: 'Open Sans', sans-serif;">Special title treatment</h5>
-              <p class="card-text" style="font-family: 'Open Sans', sans-serif;">With supporting text below as a natural lead-in to additional content.</p>
-              <a href="#" class="btn btn-primary">Go somewhere</a>
-            </div>
-          </div>
-
         </div>
+
+        <div class="col-md-4 text-center mb-3 p-2">
+          <div class="card card-body" style="font-family: 'Open Sans', sans-serif;">
+            <h5 class="card-title" style="font-family: 'Open Sans', sans-serif;">Special title treatment</h5>
+            <p class="card-text" style="font-family: 'Open Sans', sans-serif;">With supporting text below as a natural lead-in to additional content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+
+        <div class="col-md-4 text-center mb-3 p-2">
+          <div class="card card-body" style="font-family: 'Open Sans', sans-serif;">
+            <h5 class="card-title" style="font-family: 'Open Sans', sans-serif;">Special title treatment</h5>
+            <p class="card-text" style="font-family: 'Open Sans', sans-serif;">With supporting text below as a natural lead-in to additional content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+
       </div>
     </div>
   </div>
-
+  </div>
 
 </body>
