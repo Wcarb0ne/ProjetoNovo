1e1302b6505bdbc2237c48c4d820d8b60a863dc2
RapaduraDoce157 <125403663+RapaduraDoce157@users.noreply.github.com>
2023-04-26 15:51:09 -0300

    v38

--- T13/_Projeto/Cliente_Login.php
+++ T13/_Projeto/Cliente_Login.php
@@ -1,23 +1,23 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/T13/_Projeto/css/bootstrap/bootstrap.min.css">
-    <link rel="stylesheet" href="/T13/_Projeto/css/FUNCIONAAAA.css">
-    
+    <link rel="stylesheet" href="css/bootstrap/bootstrap.css">
+    <link rel="stylesheet" href="css/FUNCIONAAAA.css">
 
 
-	<title>Login</title>
- 
- </head>
+
+    <title>Login</title>
+
+</head>
 
 <body>
-<?php
+    <?php
     $mensagem = "";
-    if($_POST)
-    {
+    if ($_POST) {
         include_once('conexao.php');
 
         $loginCliente = $_POST['txtLogin'];
@@ -27,72 +27,84 @@
             "select * from Cliente where
                 login_Cliente = '$loginCliente' and
                 senha_Cliente = '$senhaCliente'
-            ");
+            "
+        );
 
-            if($sql->rowCount()==1)
-            {
-                session_start();
-                foreach($sql as $linha)
-                {
-                    $_SESSION['idCliente'] = $linha[0];
-                    $_SESSION['nomeCliente'] = $linha[1];
-                    $_SESSION['loginCliente'] = $linha[2];
-                }
+        if ($sql->rowCount() == 1) {
+            session_start();
+            foreach ($sql as $linha) {
+                $_SESSION['idCliente'] = $linha[0];
+                $_SESSION['nomeCliente'] = $linha[1];
+                $_SESSION['loginCliente'] = $linha[2];
+            }
 
-                header('Location:Login_sistema.php');
-            }
-            else
-            {
-                $mensagem = '<div class="col-sm-12 p-2"><p>Usuário ou senha inválido</p></div>';
-            }
+            header('Location:Login_sistema.php');
+        } else {
+            $mensagem = '<div class="col-sm-12 p-2"><p>Usuário ou senha inválido</p></div>';
+        }
     }
     ?>
-    
-<main class="main_content container">
-    <div class="container mt-4">
-        <div class="row">
-            <div class="col-10">
-                
-                <div class="content">            
-                    
-                    <div class="box-artigo">
-                        
-                        <form class="frmLogin" method="post" action="#">
-                        
-                            <div class="title icon icon-forward-1"> Login</div>
-                            
-                            <div class="input-container">
-                                <input id="txtlogin" class="input" type="text" id="txtLogin" placeholder=""oninput="loginValidacao"/>
-                                <div class="legenda-p"></div>
-                                <label for="nome" class="label icon icon-user-1"> Nome</label>
-                            </div>
-                            
-                            <div class="input-container">
-                                <input id="txtSenha" class="input" type="password"id="txtSenha" placeholder=""/>
-                                <div class="legenda-p"></div>
-                                <label for="email" class="label icon icon-lock-1"> Senha</label>
-                            </div>
-                            
-                            <?=$mensagem?>
 
-                            <p>Sem Cadastro <a href="Cliente_cadastro.php" class="a2">Cadastre-se</a></p>
+    <main class="main_content container">
+        <div class="container mt-4">
+            <div class="row">
+                <div class="col-10">
 
-                            <p>Gostaria de Cadastrar sua empresa ?</sub><a href="frm_Empresa.php" class="a2">Cadastre-se</a></p>
+                    <div class="content">
 
-                            <button type="text" class="btn"formaction="Login_sistema.php">Entrar</button>
-                            
-                        </form>
+                        <div class="box-artigo">
 
+                            <form class="frmLogin" method="post" action="#">
+
+                                <div class="login-box">
+
+                                    <form>
+
+                                        <div class="title icon icon-forward-1"> </div>
+
+                                
+                                        <div class="user-box">
+                                            <input id="txtLogin" type="text" name="txtLogin" required="" oninput="loginValidacao">
+                                            <label>Nome</label>
+                                        </div>
+
+                                        <div class="user-box">
+                                            <input id="txtSenha" class="" type="password" name="txtSenha" required="">
+                                            <label for="email" class="label icon icon-lock-1">SENHA</label>
+                                        </div>
+                                        <center>
+
+                                            <?= $mensagem ?>
+
+                                            <p>Sem Cadastro <a href="Cliente_cadastro.php" class="a2">CADASTRE-SE</a></p>
+
+                                            <p>Gostaria de Cadastrar sua empresa ?</sub><a href="frm_Empresa.php" class="a2">CADASTRE-SE</a></p>
+
+
+                                            <center> <a href="Login_sistema.php">ENVIAR<span></span> </a></center>
+
+                                    </form>
+
+                                </div>
+
+                        </div>
                     </div>
-                
                 </div>
             </div>
-        </div>
-    </div>
-</main>
+    </main>
 
-<script src="js/jquery.js"></script>
-<script src="js/script.js"></script>
+    <script src="js/jquery.js"></script>
+    <script src="js/script.js"></script>
+
+
+
+
+
+
+
+</form>
+</div>
 
 </body>
+
 </html>
