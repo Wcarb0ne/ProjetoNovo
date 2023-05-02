b18c84e96e3e29d0c989d37f55443d41be9a3fd5
Felipeglb <felipe.gabriel.lb.2018@gmail.com>
2023-05-02 13:37:28 -0300

    Update www_autenticar.php

--- T13/_Projeto/www_autenticar.php
+++ T13/_Projeto/www_autenticar.php
@@ -4,11 +4,18 @@ session_start();
 if($_SESSION &&($_SESSION['idCliente'])&& isset($_SESSION['nomeCliente'])&& isset($_SESSION['loginCliente']))
 
 {
+<<<<<<< Updated upstream
     $idCliente=$_SESSION['idCliente'];
     $nomeCliente=$_SESSION['nomeCliente'];
     $loginCliente=$_SESSION['loginCliente'];
+=======
+    $idParceiro=$_SESSION['idCliente'];
+    $nomeParceiro=$_SESSION['nomeCliente'];
+    $loginParceiro=$_SESSION['loginCliente'];
+>>>>>>> Stashed changes
 }
 else{
-    header('location:index.php');
+    header('location:Cliente_Usuario.php');
 }
 ?>
+
