83dc896d6ffecd32358eef78c1e82724aaf4b0fa
RapaduraDoce157 <125403663+RapaduraDoce157@users.noreply.github.com>
2023-05-05 17:15:58 -0300

    v82

---
+++ Longe/Contrato.php
@@ -0,0 +1,60 @@
+<link rel="stylesheet" href="../T13/_Projeto/css/contrato.css">
+
+
+<H1>Contrato</H1>
+<hr>
+
+<body>
+
+    <h3>Contrato de Prestação de Serviços</h3>
+    <div class="user-box">
+        <p>CONTRATANTE:</p>
+        <input type="text" name="txtLogin" required>
+
+
+        <div class="user-box">
+            <p>CONTRATADA:</p>
+            <input type="text" name="txtLogin" required>
+
+
+
+            <p>As partes acima identificadas têm, entre si, justo e acertado o presente Contrato de Prestação de Serviços de Consultoria Técnica, que se regerá pelas cláusulas seguintes e pelas condições descritas no presente.</p>
+
+            <h3>DO OBJETO DO CONTRATO</h3>
+
+            <p>Cláusula 1ª. É objeto do presente contrato, a prestação dos serviços por parte da CONTRATADA à CONTRATANTE sobre as seguintes matérias:</p>
+            <p>- Entrega de um Plano de Comunicação em até 7 dias após reunião de briefing inicial sobre o projeto.</p>
+            <p>  O(a) não se responsabiliza por eventuais problemas causados por terceiros durante o processo, tais como servidores de redes sociais.</p>
+            <p> Após o primeiro mês de projeto, os posts que estejam sujeitos a revisão por parte da CONTRATANTE contarão como a criação de novos posts, não sendo uma substituição dos posts já criados. Para revisão de posts e consequente substituição após o primeiro mês de contrato, é cobrada taxa adicional de 20% ao contrato. </p>
+
+            <h3>DAS OBRIGAÇÕES DA CONTRATADA</h3>
+
+
+            <p> Cláusula 2ª. São deveres da CONTRATADA: </p>
+            <p> a. Utilizar das técnicas disponíveis para a realização das atividades aliadas à consultoria, empregando seus melhores esforços na consecução da mesma. </p>
+            <p> b. Disponibilizar uma equipe tecnicamente capacitada para a realização de pesquisas e desenvolvimento do projeto no âmbito da matéria da consultoria devida e nomear um coordenador desta equipe, responsável pela administração das atividades.</p>
+            <p>  c. Fornecer equipamentos, laboratórios, dependências e serviços que se fizerem necessários para a execução da consultoria, mediante remuneração.</p>
+            <p> d. Administrar o presente contrato.</p>
+            <p> e. Arquivar os documentos derivados do presente contrato e apresentá-los quando exigidos por quem de direito.</p>
+            <p> f. Recolher tributos e contribuições previdenciárias que incidirem sobre as atividades do projeto, com recursos deste.</p>
+            
+            <h3>DAS OBRIGAÇÕES DA CONTRATANTE</h3>
+
+            <p> Cláusula 3ª. São direitos e deveres da CONTRATANTE: </p>
+            <p> a. Realizar o pagamento conforme disposto na cláusula 4ª deste contrato.</p>
+            <p> b. Participar, através de pessoa especialmente credenciada, das reuniões referentes a este contrato.</p>
+            <p> c. Receber relatórios dos trabalhos, na forma e datas estabelecidas neste contrato.</p>
+            <p> d. Arcar com custos de reuniões presenciais na sede da CONTRATANTE, caso seja requisitada a participação da CONTRATADA. Tais reuniões deverão acontecer caso exista comum acordo entre as partes.</p>
+            <p> e. Designar uma responsável por estar em contato com a CONTRATADA para atendimento a dúvidas levantadas.</p>
+            <p> f. Fornecer logomarca em formato de imagem ou CorelDraw ou Photoshop, identidade visual disponível e conteúdo institucional que for necessário para as publicações na página (que não estejam no website).</p>
+            
+            <h3>DO CUSTO E DA FORMA DE PAGAMENTO</h3>
+
+            <p> a.Cláusula 4ª. O desenvolvimento completo do projeto por parte da CONTRATADA possui custo mensal de R$ R$ 0,00 a ser pago pela CONTRATANTE à CONTRATADA no décimo dia útil de cada mês, iniciando-se o pagamento em , até completarem 3 mensalidades. </p>
+            <p>  </p>
+            <p>  </p>
+            <p>  </p>
+
+
+
+</body>
--- T13/_Projeto/index.php
+++ T13/_Projeto/index.php
@@ -9,7 +9,7 @@
   <link rel="stylesheet" href="css/bootstrap/bootstrap.css">
   <link rel="stylesheet" href="css/bootstrap/bootstrap.min.css">
   <link rel="stylesheet" href="css/landingPage.css">
-  <link rel="stylesheet" href="css/icon.css">
+  
 
       <!--Redes Sociais--><link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css'>
 
