d9b3c9a1bac2714cfe0eaa6b2ebec49b6d14431c
RapaduraDoce157 <125403663+RapaduraDoce157@users.noreply.github.com>
2023-05-17 17:15:08 -0300

    v118
    felipe corno

--- T13/_Projeto/_Home.php
+++ T13/_Projeto/_Home.php
@@ -30,28 +30,19 @@
        include('Conexao.php');
     
     ?>
+<style>
+
+</style>
+
+<body>
+  
 
 <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <img src="css/img/528812.jpg" class="bobo" alt="...">
+      <img src="css/img/essa copiar poha.jpg" class="bobo" alt="...">
     </div>
-    <div class="carousel-item">
-      <img src="..." class="d-block w-100" alt="...">
-    </div>
-    <div class="carousel-item">
-      <img src="..." class="d-block w-100" alt="...">
-    </div>
-  </div>
-  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Previous</span>
-  </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Next</span>
-  </button>
-</div>
+  
         </div>
         <div class="col-sm-3"></div>
     </div>
@@ -111,4 +102,4 @@
 </div>
 
 <?php include_once('_footer.php') ?>
-
+</body>
